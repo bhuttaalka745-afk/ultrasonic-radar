4f740e227ca03f88117e1d0dfbba1aff99809000
bhuttaalka745-afk <bhuttaalka745@gmail.com>
2026-02-11 15:55:18 +0530

    comment

--- sketch_feb10d.ino
+++ sketch_feb10d.ino
@@ -5,6 +5,7 @@ const int echoPin = 16;
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+// comment
 #include <Servo.h>
 Servo myservo;    
 
